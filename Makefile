d0e3d82acbc363277dbd1967344d7226f194eb40
Nedya Prakasa <nedya.prakasa@tokopedia.com>
2020-11-18 16:46:11 +0700

    fix(repo): fixing Makefile command

--- Makefile
+++ Makefile
@@ -1,5 +1,13 @@
 timeout-test-with:
-	@ for ((i=1;i<=100;i++)); do   curl -v -m 1 "localhost:8080"; done
+	( \
+		for ((i=1;i<=100;i++)); \
+		do curl -v -m 1 "localhost:8080"; \
+		done; \
+	)
 
 timeout-test-no:
-	@ for ((i=1;i<=100;i++)); do   curl -v "localhost:8080"; done
+	( \
+		for ((i=1;i<=100;i++)); \
+		do  curl -v "localhost:8080"; \
+		done; \
+	)
--- timeout/server.js
+++ timeout/server.js
@@ -1,5 +1,6 @@
 // test this node with bash for ((i=1;i<=100;i++)); do   curl -v "localhost:8080" --connect-timeout 1; done
 const http = require('http');
+const https = require('https');
 
 const array = [];
 
@@ -14,9 +15,34 @@ function fibo(n) {
   return fibo(n - 2) + fibo(n - 1);
 }
 
+function callApi() {
+  https.get('https://jsonplaceholder.typicode.com/todos/1', (res) => {
+    const { statusCode } = res;
+
+    console.log(statusCode);
+
+    if (statusCode !== 200) {
+      console.log('Error call API!');
+      return '';
+    }
+    
+    let rawData = '';
+    res.on('data', (chunk) => { rawData += chunk; });
+    res.on('end', () => {
+      try {
+        const parsedData = JSON.parse(rawData);
+        console.log(parsedData);
+      } catch (e) {
+        console.error(e.message);
+      }
+    });
+  });
+}
+
 //create a server object:
 http
   .createServer(function(req, res) {
+    callApi();
     setInterval(() => {
       const rand = Math.random();
       const fiboz = fibo(getRandomInt(0, 10));
